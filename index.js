d299efd2d50f6285a36cf38905de5581bbe3908c
AlexGaillard <alex@radish.health>
2023-01-08 10:47:40 -0500

    Change tac

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-setInterval(() => {
-  console.log("hit");
-}, 1000);
+const express = require("express");
+const app = express();
+app.all("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Yo!");
+});
+app.listen(process.env.PORT || 3000);
